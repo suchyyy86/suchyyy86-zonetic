514abbe7636ce64191690e38b4ceb932ac8e7c31
Mariusz Suchanek <mariusz.suchanek@railsformers.com>
2026-02-06 15:56:40 +0100

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/suchyyy86-zonetic-group-Public/',
+    base: '/suchyyy86-zonetic/',
     server: {
       port: 3000,
       host: '0.0.0.0',

86e9947af7d489bb9c52b4b0e518552cda642d20
Mariusz Suchanek <mariusz.suchanek@railsformers.com>
2026-02-14 22:40:45 +0100

    debug: Add error logging to api/send.ts

--- api/send.ts
+++ api/send.ts
@@ -25,6 +25,7 @@ export default async function handler(req, res) {
             });
 
             if (error) {
+                console.error('Resend API Error:', error);
                 return res.status(400).json({ error });
             }
 
